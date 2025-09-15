0903e7f5f03ff70d7b405ac7ab7de32a42406046
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-09-15 18:32:42 -0300

    Feat: Parking
    Add full functions about ParkingSystem

--- Models/Parking.cs
+++ Models/Parking.cs
@@ -14,10 +14,45 @@ namespace ParkingSystem.Models
         {
             System.Console.WriteLine("Type vehicle plate to park: ");
             string plate = Console.ReadLine();
+            if (!string.IsNullOrEmpty(plate))
+                _vehicles.Add(plate);
         }
         public void RemoveVehicle()
-        { }
+        {
+            System.Console.WriteLine("Type vehicle plate to remove: ");
+            string plate = Console.ReadLine();
+            if (_vehicles.Any(v => v.ToUpper() == plate.ToUpper()))
+            {
+                int hoursQuantity = 0;
+                System.Console.WriteLine("");
+                string hours = Console.ReadLine();
+                decimal totalCostVehicle = _initPrice + _hourPrice * hoursQuantity;
+                if (!string.IsNullOrEmpty(plate))
+                {
+                    _vehicles.Remove(plate);
+                    System.Console.WriteLine("Vehicle removed successfully");
+
+                }
+                else
+                {
+                    System.Console.WriteLine("Unable to remove vehicle");
+                }
+            }
+        }
         public void ListVehicles()
-        {}
+        {
+            if (_vehicles.Count > 0)
+            {
+                System.Console.WriteLine("List of parking vehicles: ");
+                foreach (var vehicle in _vehicles)
+                {
+                    System.Console.WriteLine($"{vehicle}");
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("Do not have any vehicle parked here");
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,49 @@
-﻿
+﻿using System.Text;
+using ParkingSystem.Models;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+decimal initialPrice;
+decimal hourPrice;
+
+System.Console.WriteLine("Welcome to parking system of Codetech Software Solutions!\n Type initial price: ");
+initialPrice = Convert.ToDecimal(Console.ReadLine());
+
+System.Console.WriteLine("Now, type hour price: ");
+hourPrice = Convert.ToDecimal(Console.ReadLine());
+
+Parking parking = new Parking(initPrice: initialPrice, hourPrice: hourPrice);
+string option = string.Empty;
+
+bool showMenu = true;
+while (showMenu)
+{
+  Console.Clear();
+  System.Console.WriteLine("Type your option: ");
+  System.Console.WriteLine("1 - Add Vehicle");
+  System.Console.WriteLine("2 - Remove Vehicle");
+  System.Console.WriteLine("3 - List Vehicles");
+  System.Console.WriteLine("4 - Exit");
+  option = Console.ReadLine();
+  switch (option)
+  {
+    case "1":
+      parking.AddVehicle();
+      break;
+    case "2":
+      parking.RemoveVehicle();
+      break;
+    case "3":
+      parking.ListVehicles();
+      break;
+    case "4":
+      showMenu = false;
+      break;
+    default:
+      System.Console.WriteLine("Invalid Option");
+      break;
+  }
+  System.Console.WriteLine("Type any key to continue");
+  Console.ReadLine();
+}
+System.Console.WriteLine("System was ended");
